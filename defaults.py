c00d585f9e147cfaa10b2b07db55bbdfd3b8c614
Shaan Sekhon <33736201+sskhon-2014@users.noreply.github.com>
2018-06-28 11:26:34 -0700

    Add files via upload

---
+++ defaults.py
@@ -0,0 +1,8 @@
+file_utr3 = "Data/genomes/refseq_3utr.bed"
+file_refseq = "Data/genomes/refseq_names.txt"
+file_targetscan = "Data/genomes/Targetscan7_fixed.bed"
+file_refgenebed = 'Data/genomes/refGene.bed'
+default_track_folder = "Data/GRAPHY_TEST/"
+
+# Placeholder defaults
+placeholder_file_bedfile = "Data/RK53/mmu-miR-29a-3p.bed"
